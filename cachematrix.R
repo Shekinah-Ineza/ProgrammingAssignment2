515c0cad086e3abfbe8d42c32fc036a0577ef161
shekinah <m.ineza1@alustudent.com>
2026-09-13 13:42:13 +0200

    Rename variable 'm' to 'inv' in makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) { m <- NULL
+makeCacheMatrix <- function(x = matrix()) { inv <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
